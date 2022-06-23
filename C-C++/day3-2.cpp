4c18cd2484961c9195328d1f86b2eabda5c2089d
3DRX <kjy20030228@icloud.com>
2022-06-23 14:47:41 +0800

    finish day3-2

--- C-C++/day3-2.cpp
+++ C-C++/day3-2.cpp
@@ -2,7 +2,6 @@
 #include <cstring>
 #include <cstdlib>
 #include <cmath>
-#include <pthread.h>
 
 #define LINE 1000
 #define LENGTH 12
@@ -11,51 +10,144 @@ int convertBinaryToDecimal(long n);
 
 int main(void)
 {
-    // 仅仅把part1的代码粘了过来
     char data[LINE][LENGTH+1];
-    char gamma[LENGTH+1];
-    char epsilon[LENGTH+1];
-    int zero = 0;
-    int one = 0;
-    int index = 0;
+    int co2[LINE];
+    int oxygen[LINE];
+    char co2res[LENGTH+1] = {'\0'};
+    char oxygenres[LENGTH+1] = {'\0'};
     for (int i = 0; i < LINE; i++) {
+        co2[i] = 1;
+        oxygen[i] = 1;
         scanf("%s", data[i]);
     }
-    for (index = 0; index < LENGTH; index++) {
-        zero = 0;
-        one = 0;
+    // {{{find oxygen
+    int count1 = 0;
+    int count0 = 0;
+    for (int j = 0; j < LENGTH; j++) {
+        count0 = 0;
+        count1 = 0;
         for (int i = 0; i < LINE; i++) {
-            if (data[i][index] == '0') {
-                zero++;
+            if (oxygen[i] == -1) {
+                continue;
             }
-            else if (data[i][index] == '1') {
-                one++;
+            if (data[i][j] == '1') {
+                count1++;
             }
-            else {
-                fprintf(stderr, "char other than 0 or 1: %d\n",
-                        data[i][index]);
-                exit(EXIT_FAILURE);
+            else if (data[i][j] == '0'){
+                count0++;
             }
         }
-        if (zero > one) {
-            gamma[index] = '0';
-            epsilon[index] = '1';
+        if (count1 + count0 == 2) { // only 2 bit strings left
+            for (int k = 0; k < LINE; k++) {
+                if (oxygen[k] == 1 && data[k][j] == '1') {
+                    strcpy(oxygenres, data[k]);
+                    break;
+                }
+            }
+            break;
         }
-        else if (zero < one) {
-            gamma[index] = '1';
-            epsilon[index] = '0';
+        else if (count1 >= count0) { // more 1 or a tie
+            for (int i = 0; i < LINE; i++) {
+                if (oxygen[i] == -1) {
+                    continue;
+                }
+                if (data[i][j] != '1') {
+                    oxygen[i] = -1;
+                    // remove the ones with 0 out of
+                    // oxygen list
+                }
+            }
         }
-        else {
-            fprintf(stderr, "same amount of 1 and 0\n");
+        else{ // more 0
+            for (int i = 0; i < LINE; i++) {
+                if (oxygen[i] == -1) {
+                    continue;
+                }
+                if (data[i][j] != '0') {
+                    oxygen[i] = -1;
+                    // remove the ones with 1 out of
+                    // oxygen list
+                }
+            }
         }
     }
-    // convert binary into decimal
-    long decGamma, decEpsilon;
-    decGamma = atol(gamma);
-    decEpsilon = atol(epsilon);
-    printf("%d\n",
-            convertBinaryToDecimal(decGamma) * convertBinaryToDecimal(decEpsilon));
-    return 0;
+    // }}}
+    // {{{find co2
+    for (int j = 0; j < LENGTH; j++) {
+        // ------------
+        printf("j:%d\n",j);
+        for (int i = 0; i < LINE; i++) {
+            if (co2[i] == 1) {
+                printf("%s\n",data[i]);
+            }
+        }
+        // ------------
+        count0 = 0;
+        count1 = 0;
+        for (int i = 0; i < LINE; i++) {
+            if (co2[i] == -1) {
+                continue;
+            }
+            if (data[i][j] == '1') {
+                count1++;
+            }
+            else if (data[i][j] == '0'){
+                count0++;
+            }
+        }
+        printf("count1:%d\ncount0:%d\n",count1,count0);
+        printf("-------------\n");
+        if (count1 + count0 == 2) { // only 2 bit strings left
+            for (int k = 0; k < LINE; k++) {
+                if (co2[k] == 1 && data[k][j] == '0') {
+                    printf("hello\n");
+                    strcpy(co2res, data[k]);
+                    break;
+                }
+            }
+            break;
+        }
+        else if (count1 + count0 == 1) {
+            for (int k = 0; k < LINE; k++) {
+                if (co2[k] == 1 ) {
+                    printf("hello\n");
+                    strcpy(co2res, data[k]);
+                    break;
+                }
+            }
+        }
+        else if (count1 < count0) { // less 1
+            for (int i = 0; i < LINE; i++) {
+                if (co2[i] == -1) {
+                    continue;
+                }
+                if (data[i][j] != '1') {
+                    co2[i] = -1;
+                    // remove the ones with 0 out of
+                    // co2 list
+                }
+            }
+        }
+        else{ // less 0 or a tie
+            for (int i = 0; i < LINE; i++) {
+                if (co2[i] == -1) {
+                    continue;
+                }
+                if (data[i][j] != '0') {
+                    co2[i] = -1;
+                    // remove the ones with 1 out of
+                    // co2 list
+                }
+            }
+        }
+    }
+    // }}}
+    long decCo2, decOxygen;
+    decCo2 = atol(co2res);
+    decOxygen = atol(oxygenres);
+    printf("%s\n%s\n",co2res,oxygenres);
+    printf("\n===========ANSWER===========\n%d\n",
+            convertBinaryToDecimal(decCo2) * convertBinaryToDecimal(decOxygen));
 }
 
 int convertBinaryToDecimal(long n)
