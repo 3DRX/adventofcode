6b2afcd860970674b5905025f4e7b432bd4be1fa
3DRX <kjy20030228@icloud.com>
2022-12-04 16:02:11 +0800

    finish day 4

---
+++ 2022/4-1.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+typedef struct _S {
+    int A_low;
+    int A_high;
+    int B_low;
+    int B_high;
+} S;
+
+void input(vector<S>* LI);
+int  ans(vector<S>* LI);
+
+int main(void)
+{
+    vector<S>* LI = new vector<S>;
+    input(LI);
+    std::cout << ans(LI) << std::endl;
+    return 0;
+}
+
+void input(vector<S>* LI)
+{
+    while (1) {
+        S newNode;
+        scanf("%d", &newNode.A_low);
+        if (newNode.A_low == -1) {
+            return;
+        }
+        getchar();
+        scanf("%d", &newNode.A_high);
+        getchar();
+        scanf("%d", &newNode.B_low);
+        getchar();
+        scanf("%d", &newNode.B_high);
+        getchar();
+        LI->push_back(newNode);
+    }
+}
+
+int ans(vector<S>* LI)
+{
+    int ans = 0;
+    for (auto i : *LI) {
+        if (i.A_low <= i.B_low && i.A_high >= i.B_high) {
+            ans++;
+        }
+        else if (i.B_low <= i.A_low && i.B_high >= i.A_high) {
+            ans++;
+        }
+    }
+    return ans;
+}
