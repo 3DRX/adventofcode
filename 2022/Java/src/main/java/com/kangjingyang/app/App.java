5da1de17bcc267d21b9ae1255c304b3a088b812c
3DRX <kjy20030228@icloud.com>
2023-01-06 21:11:27 +0800

    finish day1 in Java

---
+++ 2022/Java/src/main/java/com/kangjingyang/app/App.java
@@ -0,0 +1,38 @@
+package com.kangjingyang.app;
+
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+
+public class App {
+    public static void main(String[] args) {
+        String filename;
+        if (args[0].equals("test")) {
+            filename = "../test.io";
+        } else {
+            filename = "../input.io";
+        }
+        ArrayList<String> Lines = new ArrayList<String>();
+        try {
+            File input_file = new File(filename);
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(input_file));
+            BufferedReader br = new BufferedReader(reader);
+            String line = "";
+            while (true) {
+                line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                Lines.add(Lines.size(), line);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // --------------------------------
+        // day1.phase1(Lines);
+        day1.phase2(Lines);
+    }
+}
---
+++ 2022/Java/src/main/java/com/kangjingyang/app/day1.java
@@ -0,0 +1,46 @@
+package com.kangjingyang.app;
+
+import java.util.ArrayList;
+
+public class day1 {
+    public static void phase1(ArrayList<String> Lines) {
+        int res = 0;
+        int current_elf = 0;
+        for (String line : Lines) {
+            if (line.equals("")) {
+                if (res < current_elf) {
+                    res = current_elf;
+                }
+                current_elf = 0;
+            } else {
+                int current_line = Integer.parseInt(line);
+                current_elf += current_line;
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static void phase2(ArrayList<String> Lines) {
+        int[] res = new int[3];
+        int current_elf = 0;
+        for (String line : Lines) {
+            if (line.equals("")) {
+                if (res[0] < current_elf && res[1] >= current_elf) {
+                    res[0] = current_elf;
+                } else if (res[1] < current_elf && res[2] >= current_elf) {
+                    res[0] = res[1];
+                    res[1] = current_elf;
+                } else if (res[2] < current_elf) {
+                    res[0] = res[1];
+                    res[1] = res[2];
+                    res[2] = current_elf;
+                }
+                current_elf = 0;
+            } else {
+                int current_line = Integer.parseInt(line);
+                current_elf += current_line;
+            }
+        }
+        System.out.println(res[0] + res[1] + res[2]);
+    }
+}
