fc0390ff0255f52994c8c44e75864e88cc0cc5aa
3DRX <kjy20030228@icloud.com>
2023-10-04 19:55:08 +0800

    reduce 2 queues into one

--- 2022/C++/12-1.cpp
+++ 2022/C++/12-1.cpp
@@ -56,79 +56,58 @@ void display(vector<vector<pix>> *L) {
   }
 }
 
-#define L(i, j) (*L)[i][j]
-
 int ans(vector<vector<pix>> *L, pos start) {
+#define L(i, j) (*L)[i][j]
   int X = L->size();
   int Y = (*L)[0].size();
-  int step = 0;
-  queue<pos> *current_layer = new queue<pos>;
-  queue<pos> *next_layer = new queue<pos>;
-  current_layer->push(start);
+  queue<pos> q;
+  q.push(start);
   L(start.x, start.y).enqueued = true;
   char max_c = 'a' - 1;
-  while (true) {
-    cout << "\riteration: " << step << " ";
-    cout << "max_c: " << max_c << " ";
-    cout << "current_layer: " << current_layer->size() << std::flush;
-    if (max_c >= 'y') {
-      cout << endl;
-      display(L);
+  while (q.size() != 0) {
+    pos p = q.front();
+    q.pop();
+    L(p.x, p.y).visited = true;
+    if (L(p.x, p.y).value > max_c) {
+      max_c = L(p.x, p.y).value;
     }
-    while (current_layer->size() != 0) {
-      pos p = current_layer->front();
-      current_layer->pop();
-      L(p.x, p.y).visited = true;
-      if (L(p.x, p.y).value > max_c) {
-        max_c = L(p.x, p.y).value;
-      }
-      if (L(p.x, p.y).value == 'z' + 1) {
-        delete current_layer;
-        delete next_layer;
-        return L(p.x, p.y).distance;
-      }
-      // get adjacent position of p
-      if (p.x != 0 && !L(p.x - 1, p.y).visited && !L(p.x - 1, p.y).enqueued &&
-          (L(p.x - 1, p.y).value <= L(p.x, p.y).value + 1)) {
-        next_layer->push({p.x - 1, p.y});
+    if (L(p.x, p.y).value == 'z' + 1) {
+      return L(p.x, p.y).distance;
+    }
+    if (p.x != 0 && (L(p.x - 1, p.y).value <= L(p.x, p.y).value + 1)) {
+      if (!L(p.x - 1, p.y).visited && !L(p.x - 1, p.y).enqueued) {
+        q.push({p.x - 1, p.y});
         L(p.x - 1, p.y).enqueued = true;
-        L(p.x - 1, p.y).distance =
-            std::min(L(p.x - 1, p.y).distance, L(p.x, p.y).distance + 1);
-        // cout << "push: " << p.x - 1 << " " << p.y << endl;
       }
-      if (p.x != X - 1 && !L(p.x + 1, p.y).visited &&
-          !L(p.x + 1, p.y).enqueued &&
-          (L(p.x + 1, p.y).value <= L(p.x, p.y).value + 1)) {
-        next_layer->push({p.x + 1, p.y});
-        L(p.x + 1, p.y).enqueued = true;
-        L(p.x + 1, p.y).distance =
-            std::min(L(p.x + 1, p.y).distance, L(p.x, p.y).distance + 1);
-        // cout << "push: " << p.x + 1 << " " << p.y << endl;
-      }
-      if (p.y != 0 && !L(p.x, p.y - 1).visited && !L(p.x, p.y - 1).enqueued &&
-          (L(p.x, p.y - 1).value <= L(p.x, p.y).value + 1)) {
-        next_layer->push({p.x, p.y - 1});
-        L(p.x, p.y - 1).enqueued = true;
-        L(p.x, p.y - 1).distance =
-            std::min(L(p.x, p.y - 1).distance, L(p.x, p.y).distance + 1);
-        // cout << "push: " << p.x << " " << p.y - 1 << endl;
-      }
-      if (p.y != Y - 1 && !L(p.x, p.y + 1).visited &&
-          !L(p.x, p.y + 1).enqueued &&
-          (L(p.x, p.y + 1).value <= L(p.x, p.y).value + 1)) {
-        next_layer->push({p.x, p.y + 1});
-        L(p.x, p.y + 1).enqueued = true;
-        L(p.x, p.y + 1).distance =
-            std::min(L(p.x, p.y + 1).distance, L(p.x, p.y).distance + 1);
-        // cout << "push: " << p.x << " " << p.y + 1 << endl;
-      }
+      L(p.x - 1, p.y).distance =
+          std::min(L(p.x - 1, p.y).distance, L(p.x, p.y).distance + 1);
     }
-    // swap
-    auto t = current_layer;
-    current_layer = next_layer;
-    next_layer = t;
-    step++;
+    if (p.x != X - 1 && (L(p.x + 1, p.y).value <= L(p.x, p.y).value + 1)) {
+      if (!L(p.x + 1, p.y).visited && !L(p.x + 1, p.y).enqueued) {
+        q.push({p.x + 1, p.y});
+        L(p.x + 1, p.y).enqueued = true;
+      }
+      L(p.x + 1, p.y).distance =
+          std::min(L(p.x + 1, p.y).distance, L(p.x, p.y).distance + 1);
+    }
+    if (p.y != 0 && (L(p.x, p.y - 1).value <= L(p.x, p.y).value + 1)) {
+      if (!L(p.x, p.y - 1).visited && !L(p.x, p.y - 1).enqueued) {
+        q.push({p.x, p.y - 1});
+        L(p.x, p.y - 1).enqueued = true;
+      }
+      L(p.x, p.y - 1).distance =
+          std::min(L(p.x, p.y - 1).distance, L(p.x, p.y).distance + 1);
+    }
+    if (p.y != Y - 1 && (L(p.x, p.y + 1).value <= L(p.x, p.y).value + 1)) {
+      if (!L(p.x, p.y + 1).visited && !L(p.x, p.y + 1).enqueued) {
+        q.push({p.x, p.y + 1});
+        L(p.x, p.y + 1).enqueued = true;
+      }
+      L(p.x, p.y + 1).distance =
+          std::min(L(p.x, p.y + 1).distance, L(p.x, p.y).distance + 1);
+    }
   }
+  return INT_MAX;
 }
 
 int main(void) {
